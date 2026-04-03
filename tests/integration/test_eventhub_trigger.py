bbc9ce609b2703feaa8ead236d1d665e25f2f984
Ahmed Muhsin <ahmedmuhsin@microsoft.com>
2026-04-02 19:08:36 -0500

    Simplify test env vars: use UseDevelopmentStorage=true where possible
    - Event Hub test: switch to UseDevelopmentStorage=true for checkpoint storage
    - Blob test: keep explicit Azurite connection string (Go azblob SDK has
      API version compat issues with UseDevelopmentStorage=true on older Azurite)

--- tests/integration/test_eventhub_trigger.py
+++ tests/integration/test_eventhub_trigger.py
@@ -11,17 +11,8 @@ EH_CONN_STR = (
     "UseDevelopmentEmulator=true;"
 )
 
-AZURITE_CONN_STR = (
-    "DefaultEndpointsProtocol=http;"
-    "AccountName=devstoreaccount1;"
-    "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
-    "BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
-    "QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;"
-    "TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
-)
-
 ENV = {
-    "AzureWebJobsStorage": AZURITE_CONN_STR,
+    "AzureWebJobsStorage": "UseDevelopmentStorage=true",
     "FUNCTIONS_WORKER_RUNTIME": "golang",
     "EventHubConnection": EH_CONN_STR,
 }
